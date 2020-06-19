454d7c272a7a39103998f0e1214091f528e56778
Israel <root@boltage>
2020-06-19 17:14:32 +0300

    Add edit button and some css

--- ezeki.js
+++ ezeki.js
@@ -9,15 +9,27 @@ for(const npc of npcs){
     const npcCell = document.createElement('td');
     npcCell.appendChild(document.createTextNode(npc.name));
     npcCell.addEventListener('click', async() => {
+
         const display = document.createElement('div');
         display.classList.add('display');
+        display.classList.add('loading');
+
         const closer = document.createElement('div');
         closer.addEventListener('click', () => display.parentNode.removeChild(display));
         closer.classList.add('closer');
-        closer.appendChild(document.createTextNode('[close]'));
+        closer.appendChild(document.createTextNode('[סגור]'));
         display.appendChild(closer);
+
+        const editor = document.createElement('div');
+        editor.addEventListener('click', () =>
+            document.location.href = 'https://github.com/israellevin/ezeki/edit/master/npc/' + npc.name);
+        editor.classList.add('editor');
+        editor.appendChild(document.createTextNode('[ערוך]'));
+        display.appendChild(editor);
+
         document.body.appendChild(display);
         display.appendChild(document.createTextNode(await gettext('master/npc/' + npc.name)));
+        display.classList.remove('loading');
     });
     npcRow.appendChild(npcCell);
 }
