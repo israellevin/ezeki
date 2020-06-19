62fe90f9d099bf166de4391d4b84601e121beef6
Israel <root@boltage>
2020-06-19 15:52:44 +0300

    Read repo data from proxy

--- ezeki.js
+++ ezeki.js
@@ -1,6 +1,17 @@
 // jshint esversion: 8
 (() => {'use strict';
-const baseurl = 'https://api.github.com/repos/israellevin/ezeki/contents';
-const get = async url => await (await fetch(url)).text();
-(async() => {console.log(await get(baseurl + '/npc'));})();
+const getjson = async path => await (await fetch('https://ezeki.pythonanywhere.com/' + path)).json();
+const gettext = async path => await (await fetch('https://raw.githubusercontent.com/israellevin/ezeki/' + path)).text();
+(async() => {
+let npcs = await getjson('npc');
+const npcRow = document.createElement('tr');
+for(let npc of npcs){
+    const npcCell = document.createElement('td');
+    npcCell.appendChild(document.createTextNode(npc.name));
+    npcCell.addEventListener('click', async() => console.log(await gettext('master/npc/' + npc.name)))
+    npcRow.appendChild(npcCell);
+}
+document.querySelector('div.npc table.npc').appendChild(npcRow);
+console.log(npcs);
+})();
 })();
