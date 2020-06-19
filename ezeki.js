fc49ad3ab4933f5dcad56a553d730e1080cf7bc5
Israel <root@boltage>
2020-06-19 17:54:36 +0300

    Add opinions

--- ezeki.js
+++ ezeki.js
@@ -1,38 +1,57 @@
 // jshint esversion: 8
-(() => {'use strict';
+(async() => {'use strict';
+
 const getjson = async path => await (await fetch('https://ezeki.pythonanywhere.com/' + path)).json();
 const gettext = async path => await (await fetch('https://raw.githubusercontent.com/israellevin/ezeki/' + path)).text();
-(async() => {
+const displayPage = async source => {
+    const display = document.createElement('div');
+    display.classList.add('display');
+    display.classList.add('loading');
+
+    const closer = document.createElement('div');
+    closer.addEventListener('click', () => display.parentNode.removeChild(display));
+    closer.classList.add('closer');
+    closer.appendChild(document.createTextNode('[סגור]'));
+    display.appendChild(closer);
+
+    const editor = document.createElement('div');
+    editor.addEventListener('click', () =>
+        document.location.href = 'https://github.com/israellevin/ezeki/edit/master/' + source);
+    editor.classList.add('editor');
+    editor.appendChild(document.createTextNode('[ערוך]'));
+    display.appendChild(editor);
+
+    document.body.appendChild(display);
+    const text = await gettext('master/' + source);
+    if(text === '404: Not Found'){
+        document.location.href = 'https://github.com/israellevin/ezeki/new/master/?filename=' + source;
+    }
+    display.appendChild(document.createTextNode(text));
+    display.classList.remove('loading');
+};
+
+console.log(await gettext('npc/nosuch'));
 let npcs = await getjson('npc');
 const npcRow = document.createElement('tr');
+const opinionRows = document.createElement('tbody');
 for(const npc of npcs){
     const npcCell = document.createElement('td');
     npcCell.appendChild(document.createTextNode(npc.name));
-    npcCell.addEventListener('click', async() => {
-
-        const display = document.createElement('div');
-        display.classList.add('display');
-        display.classList.add('loading');
-
-        const closer = document.createElement('div');
-        closer.addEventListener('click', () => display.parentNode.removeChild(display));
-        closer.classList.add('closer');
-        closer.appendChild(document.createTextNode('[סגור]'));
-        display.appendChild(closer);
-
-        const editor = document.createElement('div');
-        editor.addEventListener('click', () =>
-            document.location.href = 'https://github.com/israellevin/ezeki/edit/master/npc/' + npc.name);
-        editor.classList.add('editor');
-        editor.appendChild(document.createTextNode('[ערוך]'));
-        display.appendChild(editor);
-
-        document.body.appendChild(display);
-        display.appendChild(document.createTextNode(await gettext('master/npc/' + npc.name)));
-        display.classList.remove('loading');
-    });
+    npcCell.addEventListener('click', () => displayPage('npc/' + npc.name));
     npcRow.appendChild(npcCell);
+
+    const opinionRow = document.createElement('tr');
+    for(const other of npcs){
+        const opinionCell = document.createElement('td');
+        opinionCell.appendChild(document.createTextNode("מה " + npc.name + " חושב/ת על " + other.name));
+        opinionCell.addEventListener('click', () => displayPage('opinions/' + npc.name + '.' + other.name));
+        opinionRow.appendChild(opinionCell);
+    }
+    opinionRows.appendChild(opinionRow);
 }
-document.querySelector('div.npc table.npc').appendChild(npcRow);
-})();
+const table = document.querySelector('div.npc table.npc');
+const tableHead = document.createElement('thead');
+tableHead.appendChild(npcRow);
+table.appendChild(tableHead);
+table.appendChild(opinionRows);
 })();
