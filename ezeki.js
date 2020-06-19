d61bd8ff7619f09989b4dd9eda8f207b1f07a551
Israel <root@boltage>
2020-06-19 16:26:55 +0300

    Add display

--- ezeki.js
+++ ezeki.js
@@ -5,13 +5,22 @@ const gettext = async path => await (await fetch('https://raw.githubusercontent.
 (async() => {
 let npcs = await getjson('npc');
 const npcRow = document.createElement('tr');
-for(let npc of npcs){
+for(const npc of npcs){
     const npcCell = document.createElement('td');
     npcCell.appendChild(document.createTextNode(npc.name));
-    npcCell.addEventListener('click', async() => console.log(await gettext('master/npc/' + npc.name)))
+    npcCell.addEventListener('click', async() => {
+        const display = document.createElement('div');
+        display.classList.add('display');
+        const closer = document.createElement('div');
+        closer.addEventListener('click', () => display.parentNode.removeChild(display));
+        closer.classList.add('closer');
+        closer.appendChild(document.createTextNode('[close]'));
+        display.appendChild(closer);
+        document.body.appendChild(display);
+        display.appendChild(document.createTextNode(await gettext('master/npc/' + npc.name)));
+    });
     npcRow.appendChild(npcCell);
 }
 document.querySelector('div.npc table.npc').appendChild(npcRow);
-console.log(npcs);
 })();
 })();
